4d302be18841ec9a1127082b055c61447206de9d
will-sklenars <will.sklenars@gmail.com>
2016-07-05 13:05:46 +1200

    restructured to be a factory fxn

--- run-at-fps.js
+++ run-at-fps.js
@@ -1,38 +1,41 @@
-export default function Fps () {
-  this.framesCounter = 0
-  this.oldTime = 0
-  this.current = undefined
+function RunAtFps () {
+  var proto = {
+    update : function(delta, nowMsec) {
+      this.framesCounter ++
+      if (nowMsec > this.oldTime + 0.2) {
+        this.current = Math.round( this.framesCounter / ( nowMsec - this.oldTime ) )
+        this.framesCounter = 0
+        this.oldTime = nowMsec
+      }
+    },
+
+    runFunctionAtFps : function(opts) {
+      var self = this
+      opts.callCounter = opts.callCounter || 1
+
+      if (this.current >= (opts.targetFrameRate || 20)) {
+        opts.toRun(opts.args)
+      } else {
+        (opts.callCounter > 20) ? didNotReachTarget() : runLater()
+      }
+
+      function didNotReachTarget() {
+        opts.toRun(opts.args)
+        console.warn('target framerate was not reached for fxn')
+      }
+
+      function runLater () {
+        setTimeout(function () {
+          opts.callCounter ++
+          self.runFunctionAtFps(opts)
+        }, 200)
+      }
+    }
+  }
+
+  return Object.assign( Object.create(proto), {
+    framesCounter : 0,
+    oldTime : 0,
+    current : undefined
+  })
 }
-
-Fps.prototype.update = function(delta, nowMsec) {
-  this.framesCounter ++
-  if (nowMsec > this.oldTime + 0.2) {
-    this.current = Math.round( this.framesCounter / ( nowMsec - this.oldTime ) )
-    this.framesCounter = 0
-    this.oldTime = nowMsec
-  }
-};
-
-Fps.prototype.runFunctionAtFps = function(opts) {
-  var self = this
-  opts.callCounter = opts.callCounter || 1
-
-  if (this.current >= (opts.targetFrameRate || 20)) {
-    opts.toRun(opts.args)
-  } else {
-    (opts.callCounter > 20) ? didNotReachTarget() : runLater()
-  }
-
-  function didNotReachTarget() {
-    opts.toRun(opts.args)
-    console.warn('target framerate was not reached for fxn')
-  }
-
-  function runLater () {
-    setTimeout(function () {
-      opts.callCounter ++
-      self.runFunctionAtFps(opts)
-    }, 200)
-  }
-
-};
